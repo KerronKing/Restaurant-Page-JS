99bf03c62c9652e670c10702074443258509ae3f
KerronKing <king_of_palmiste@hotmail.com>
2020-01-06 17:07:32 -0400

    Fix lint error

--- webpack.config.js
+++ webpack.config.js
@@ -23,8 +23,8 @@ module.exports = {
           loader: 'file-loader',
           options: {
             name: '[name].[ext]',
-            outputPath: 'assets'
-          }
+            outputPath: 'assets',
+          },
         },
       },
     ],
