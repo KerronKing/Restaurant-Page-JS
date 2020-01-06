f17f5dbe804b9f560935435c65add90a9feac398
KerronKing <king_of_palmiste@hotmail.com>
2020-01-06 11:25:36 -0400

    Add about section render function and edit styling

--- src/about.js
+++ src/about.js
@@ -1,11 +1,14 @@
 export const aboutTab = (() => {
   const render = () => {
     const content = document.getElementById('content');
+    if (content.firstChild !== null) {
+      content.firstChild.remove();
+    }
     const aboutInfo = document.createElement('DIV');
     aboutInfo.classList.add('info');
     aboutInfo.textContent = 'King\'s Healthy Eats (KHE) is your premium one stop' + 
-    'shop for quality Caribbean cooking. Come dine with us and we promise you\'ll' + 
-    'leave in better stead than you came';
+    ' shop for quality Caribbean cooking. Come dine with us and we promise you\'ll' + 
+    ' leave in better stead than you came';
     content.appendChild(aboutInfo);
   }
   return { render };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import { nav } from "./nav.js";
+import { aboutTab } from "./about.js";
 
 nav.render();
+
+const content = document.getElementById('content');
+const about = document.getElementById('about');
+about.addEventListener('click', () => {
+  aboutTab.render()
+})
+const contact = document.getElementById('contact');
+const menu = document.getElementById('menu');
+const reserve = document.getElementById('reserve');
+const locations = document.getElementById('locations');
+
