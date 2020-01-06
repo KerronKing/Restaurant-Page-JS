2864be3720a49e3e702500af3aa9c1300ff2be16
KerronKing <king_of_palmiste@hotmail.com>
2020-01-06 12:12:44 -0400

    Add contact tab render function and styling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { nav } from "./nav.js";
 import { aboutTab } from "./about.js";
+import { contactTab } from "./contact.js";
 
 nav.render();
 
@@ -9,6 +10,9 @@ about.addEventListener('click', () => {
   aboutTab.render()
 })
 const contact = document.getElementById('contact');
+contact.addEventListener('click', () => {
+  contactTab.render()
+})
 const menu = document.getElementById('menu');
 const reserve = document.getElementById('reserve');
 const locations = document.getElementById('locations');
